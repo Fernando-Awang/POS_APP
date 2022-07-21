47fd19394af3ae644919443fd2497d29416caa7c
Fernando-Awang <fernandoawang@gmail.com>
2022-07-21 20:54:47 +0700

    model barang dan kategori barang

---
+++ app/Models/Barang.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Barang extends Model
+{
+    use HasFactory;
+    protected $table = 'barang';
+    protected $primaryKey = 'id';
+    protected $guarded = [];
+    public $timestamps = false;
+    public function kategori_barang()
+    {
+        return $this->belongsTo(KategoriBarang::class, 'kategori_barang_id', 'id');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -24,4 +24,5 @@ class User extends Authenticatable
     {
         return $this->hasOne(DetailUser::class, 'user_id', 'id');
     }
+
 }
